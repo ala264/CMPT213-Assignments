2a9b1944cd6abdee1337b591238ecc9c3fd721e0
Arman Lodhra <ala264@sfu.ca>
2026-05-26 12:31:59 -0700

    create loan info class

---
+++ A1/CMPT213A1/src/LoanInformation.java
@@ -0,0 +1,30 @@
+package A1.CMPT213A1.src;
+
+import java.time.LocalDate;
+
+public class LoanInformation {
+    public String name;
+    public LocalDate dueDate;
+    public String publisher;
+    public String loanedTo;
+
+    public LoanInformation(String name, LocalDate dueDate, String publisher, String loanedTo){
+        this.name = name;
+        this.dueDate = dueDate;
+        this.publisher = publisher;
+        this.loanedTo = loanedTo;
+    }
+
+    public LoanInformation(String name, LocalDate dueDate, String loanedTo){
+        this.name = name;
+        this.dueDate = dueDate;
+        this.publisher = "";
+        this.loanedTo = loanedTo;
+    }
+
+    @Override
+    public String toString() {
+        return "name: " + name + "dueDate: " + dueDate + "publisher: " + publisher + "loanedTo: " + loanedTo;
+    }
+
+}
